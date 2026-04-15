122df6a5ab858f985acce30359f4c8e94708e0e4
Somayeh Gulestani <somayehgulistani14@gmail.com>
2026-04-15 22:51:34 +0430

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDom from "react-dom/client";
 import "./tailwind.css";
+// import "./index.css";
 
 // import { CarRental } from "./CarRental.js";
 import App from "./Trip.js";
+// import App from "./App-1.js";
+// import App from "./weather.js";
 
 const root = ReactDom.createRoot(document.getElementById("root"));
 root.render(
