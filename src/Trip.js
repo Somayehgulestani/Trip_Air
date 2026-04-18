a4b7d2eea6b1f51ba139b6f5d854833ad0d05052
Somayeh Gulestani <somayehgulistani14@gmail.com>
2026-04-18 23:47:10 +0430

    src files

--- src/Trip.js
+++ src/Trip.js
@@ -1,117 +1,154 @@
-import { useState } from "react";
+import { set } from "date-fns";
+import { useEffect, useState } from "react";
 import {
   FaPlane,
   FaDollarSign,
   FaBullseye,
   FaGlobe,
   FaHandshake,
+  FaSadCry,
+  FaSadTear,
 } from "react-icons/fa";
-const flightsList = [
-  {
-    origin: "Herat",
-    destination: "Mazar",
-    departureTime: "8:00",
-    landing: "9:50",
-    price: 115,
-    remaindingSeats: 11,
-  },
-  {
-    origin: "Herat",
-    destination: "Mazar",
-    departureTime: "9:00",
-    landing: "10:50",
-    price: 110,
-    remaindingSeats: 5,
-  },
-  {
-    origin: "Herat",
-    destination: "Kabul",
-    departureTime: "10:00",
-    landing: "11:30",
-    price: 95,
-    remaindingSeats: 1,
-  },
-  {
-    origin: "Herat",
-    destination: "Mashhad",
-    departureTime: "10:00",
-    landing: "11:00",
-    price: 180,
-    remaindingSeats: 3,
-  },
-  {
-    origin: "Herat",
-    destination: "Mazar",
-    departureTime: "13:30",
-    landing: "14:45",
-    price: 131,
-    remaindingSeats: 31,
-  },
-  {
-    origin: "Herat",
-    destination: "Kabul",
-    departureTime: "14:00",
-    landing: "15:30",
-    price: 93,
-    remaindingSeats: 10,
-  },
-  {
-    origin: "Herat",
-    destination: "Mashhad",
-    departureTime: "14:00",
-    landing: "14:50",
-    price: 176,
-    remaindingSeats: 7,
-  },
-  {
-    origin: "Herat",
-    destination: "Kabul",
-    departureTime: "22:00",
-    landing: "23:30",
-    price: 89,
-    remaindingSeats: 9,
-  },
-  {
-    origin: "Herat",
-    destination: "Kabul",
-    departureTime: "18:30",
-    landing: "19:45",
-    price: 90,
-    remaindingSeats: 8,
-  },
-  {
-    origin: "Herat",
-    destination: "Mashhad",
-    departureTime: "18:30",
-    landing: "16:45",
-    price: 182,
-    remaindingSeats: 18,
-  },
-  {
-    origin: "Herat",
-    destination: "Mazar",
-    departureTime: "21:00",
-    landing: "23:00",
-    price: 122,
-    remaindingSeats: 6,
-  },
-  {
-    origin: "Herat",
-    destination: "Mashhad",
-    departureTime: "22:00",
-    landing: "22:50",
-    price: 168,
-    remaindingSeats: 20,
-  },
-];
+// const flightsList = [
+//   {
+//     origin: "Herat",
+//     destination: "Mazar",
+//     departureTime: "8:00",
+//     landing: "9:50",
+//     price: 115,
+//     remaindingSeats: 10,
+//   },
+//   {
+//     origin: "Herat",
+//     destination: "Mazar",
+//     departureTime: "9:00",
+//     landing: "10:50",
+//     price: 110,
+//     remaindingSeats: 5,
+//   },
+//   {
+//     origin: "Herat",
+//     destination: "Kabul",
+//     departureTime: "10:00",
+//     landing: "11:30",
+//     price: 95,
+//     remaindingSeats: 0,
+//   },
+//   {
+//     origin: "Herat",
+//     destination: "Mashhad",
+//     departureTime: "10:00",
+//     landing: "11:00",
+//     price: 180,
+//     remaindingSeats: 3,
+//   },
+//   {
+//     origin: "Herat",
+//     destination: "Mazar",
+//     departureTime: "13:30",
+//     landing: "14:45",
+//     price: 131,
+//     remaindingSeats: 31,
+//   },
+//   {
+//     origin: "Herat",
+//     destination: "Kabul",
+//     departureTime: "14:00",
+//     landing: "15:30",
+//     price: 93,
+//     remaindingSeats: 10,
+//   },
+//   {
+//     origin: "Herat",
+//     destination: "Mashhad",
+//     departureTime: "14:00",
+//     landing: "14:50",
+//     price: 176,
+//     remaindingSeats: 7,
+//   },
+//   {
+//     origin: "Herat",
+//     destination: "Kabul",
+//     departureTime: "22:00",
+//     landing: "23:30",
+//     price: 89,
+//     remaindingSeats: 0,
+//   },
+//   {
+//     origin: "Herat",
+//     destination: "Kabul",
+//     departureTime: "18:30",
+//     landing: "19:45",
+//     price: 90,
+//     remaindingSeats: 8,
+//   },
+//   {
+//     origin: "Herat",
+//     destination: "Mashhad",
+//     departureTime: "18:30",
+//     landing: "16:45",
+//     price: 182,
+//     remaindingSeats: 18,
+//   },
+//   {
+//     origin: "Herat",
+//     destination: "Mazar",
+//     departureTime: "21:00",
+//     landing: "23:00",
+//     price: 122,
+//     remaindingSeats: 30,
+//   },
+//   {
+//     origin: "Herat",
+//     destination: "Mashhad",
+//     departureTime: "22:00",
+//     landing: "22:50",
+//     price: 168,
+//     remaindingSeats: 20,
+//   },
+// ];
 
 export default function Trip() {
+  const [List, setList] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(false);
+
+  // useEffect(() => {
+  //   const handleStatus = () => console.log("changed status");
+
+  //   window.addEventListener("offline", handleStatus);
+  //   window.addEventListener("online", handleStatus);
+
+  //   return () => {
+  //     window.addEventListener("offline", handleStatus);
+  //     window.addEventListener("online", handleStatus);
+  //   };
+  // }, []);
+
+  useEffect(() => {
+    async function loadFlights() {
+      try {
+        const res = await fetch(
+          "https://69e282ab3327837a15527356.mockapi.io/flights?destination=Herat",
+        );
+        const data = await res.json();
+        console.log(data);
+
+        if (!res.ok) throw new Error("NETWORK ERROR");
+
+        setList(data);
+      } catch (error) {
+        setErrorMessage(true);
+      }
+    }
+
+    loadFlights();
+  }, []);
+
   return (
     <>
       <HeroSection />
-      <Flights />
+      <Flights List={List} errorMessage={errorMessage} />
       <CityImages />
-      <BookForm />
     </>
   );
 }
@@ -171,8 +208,7 @@ function HeroSection() {
   );
 }
 
-function Flights() {
-  const [List, setList] = useState(flightsList);
+function Flights({ List, errorMessage }) {
   const [showMore, setShowMore] = useState(false);
   const [filterFlights, setFilterFlights] = useState("All");
 
@@ -183,19 +219,20 @@ function Flights() {
 
   return (
     <>
-      <div className="bg-gradient-to-b from-blue-100 to-blue-300 pb-20">
+      <div className="bg-gradient-to-b from-violet-100 to-violet-300 pb-20">
         <div className="flex flex-wrap justify-center  gap-6 pt-12 pb-10">
-          <h2 className="w-full text-gray-700 text-5xl font-bold text-center uppercase mb-6">
+          <h2 className="w-full text-violet-950 text-5xl font-bold text-center uppercase mb-6">
             Flights
           </h2>
           <CitiesFlightsOptions
             onSetList={List}
             onSetFilterFlights={setFilterFlights}
           />
+          {errorMessage && <ErrorMessage />}
           <FlightsCities showMore={showMore} filterByCity={filterByCity} />
         </div>
         <button
-          className="bg-white  font-bold rounded-md  p-2 w-[200px] hover:bg-white transition-colors duration-300  hover:scale-105 block mx-auto"
+          className="bg-white  font-bold rounded-md  p-2 w-[200px]  transition-colors duration-300  hover:scale-105 block mx-auto"
           onClick={() => setShowMore(!showMore)}
         >
           {showMore ? "Show Less" : "Show More"}
@@ -224,7 +261,7 @@ function CityImages() {
         {cities.map((city, index) => (
           <div
             key={index}
-            className={`relative overflow-hidden rounded-xl shadow-lg  group ${index === 2 && "lg:row-span-2 "} ${index == 3 && "lg:col-span-2 "}`}
+            className={`relative overflow-hidden rounded-xl shadow-lg  group ${index === 2 && "lg:row-span-2 "} ${index === 3 && "lg:col-span-2 "}`}
           >
             <img
               src={city.images}
@@ -296,25 +333,25 @@ function CitiesFlightsOptions({ onSetFilterFlights }) {
   return (
     <div className="w-full flex justify-center  gap-3 font-semibold">
       <button
-        className="p-1 rounded-lg  bg-blue-900 text-white w-[50px]"
+        className="p-1 rounded-lg  bg-gradient-to-b from-violet-300 to-violet-500 text-white w-[50px] "
         onClick={() => onSetFilterFlights("All")}
       >
         All
       </button>
       <button
-        className="p-2 rounded-lg  bg-blue-900 text-white"
+        className=" pt-1 pb-1 rounded-lg  bg-gradient-to-b from-violet-300 to-violet-500 text-white w-[100px]"
         onClick={() => onSetFilterFlights("Kabul")}
       >
         kabul
       </button>
       <button
-        className="p-2 rounded-lg  bg-blue-900 text-white"
+        className="pt-1 pb-1 rounded-lg  bg-gradient-to-b from-violet-300 to-violet-500 text-white w-[100px]"
         onClick={() => onSetFilterFlights("Mazar")}
       >
         MazarSharif
       </button>
       <button
-        className="p-2 rounded-lg  bg-blue-900 text-white"
+        className="pt-1 pb-1 rounded-lg  bg-gradient-to-b from-violet-300 to-violet-500 text-white w-[100px]"
         onClick={() => onSetFilterFlights("Mashhad")}
       >
         Mashhad
@@ -325,9 +362,22 @@ function CitiesFlightsOptions({ onSetFilterFlights }) {
 
 function FlightsCities({ filterByCity, showMore }) {
   const [bookFlight, setBookFlights] = useState(false);
+  const [alert, setAlert] = useState(false);
   const flights = showMore ? filterByCity : filterByCity.slice(0, 3);
+
+  function handdleBookFlight(index) {
+    const remaindingSeat = flights[index].remaindingSeats;
+    if (remaindingSeat >= 1) {
+      setAlert(false);
+      setBookFlights(true);
+    } else {
+      setBookFlights(false);
+      setAlert(true);
+    }
+  }
+
   return (
-    <>
+    <div className=" relative">
       {flights.map((items, index) => {
         return (
           <div key={index} className=" w-[380px] bg-slate-100 rounded-lg">
@@ -370,7 +420,7 @@ function FlightsCities({ filterByCity, showMore }) {
               <div className="flex justify-center">
                 <button
                   className="bg-slate-400 text-zinc-700 font-extrabold rounded-md m-3   p-2 w-1/2 hover:bg-blue-500 hover:text-white transition-colors duration-300  hover:scale-105"
-                  onClick={() => setBookFlights(true)}
+                  onClick={() => handdleBookFlight(index)}
                 >
                   Buy Ticket
                 </button>
@@ -379,59 +429,218 @@ function FlightsCities({ filterByCity, showMore }) {
           </div>
         );
       })}
+      {bookFlight && <BookForm onSetBookFlights={setBookFlights} />}
+      {alert && <NoSeatAlert onSetAlert={setAlert} />}
+    </div>
+  );
+}
+
+function BookForm({ onSetBookFlights }) {
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [adults, setAdults] = useState(1);
+  const [children, setChildren] = useState(0);
+  const [infants, setInfants] = useState(0);
+  const [booked, setBooked] = useState([]);
+  const [isActive, setIsAcrive] = useState(false);
+  console.log(phone);
+
+  function handleBooked(e) {
+    e.preventDefault();
+
+    if (name.length < 3) {
+      setIsAcrive(true);
+    } else if (phone.length !== 10) {
+      setIsAcrive(true);
+    } else {
+      setBooked([
+        ...booked,
+        {
+          name: name,
+          phone: phone,
+          adults: adults,
+          children: children,
+          infants: infants,
+        },
+      ]);
+    }
+  }
+  console.log(booked);
+
+  return (
+    <div className="w-[400px] bg-purple-200 m-5 p-4 mx-auto rounded-lg absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ">
+      <FormHeadline onSetBookFlights={onSetBookFlights} />
+      <form>
+        <FormFields
+          name={name}
+          setName={setName}
+          phone={phone}
+          setPhone={setPhone}
+          isActive={isActive}
+        />
+        <NumberOfPeople
+          adults={adults}
+          setAdults={setAdults}
+          children={children}
+          setChildren={setChildren}
+          infants={infants}
+          setInfants={setInfants}
+        />
+        <TotalPrice />
+        <button
+          className="bg-white text-sm font-semibold rounded-xl p-2 w-full mt-8  "
+          onClick={(e) => handleBooked(e)}
+        >
+          Book
+        </button>
+      </form>
+    </div>
+  );
+}
+
+function FormHeadline({ onSetBookFlights }) {
+  return (
+    <div className="flex  justify-between">
+      <h3 className="font-medium  text-2xl mt-2 mb-4">
+        Please Fill Out The Form
+      </h3>
+      <button
+        className="text-2xl font-bold  rounded-sm p-1"
+        onClick={() => onSetBookFlights(false)}
+      >
+        ✕
+      </button>
+    </div>
+  );
+}
+
+function FormFields({ name, setName, phone, setPhone, isActive }) {
+  return (
+    <>
+      <label className="block text-sm font-semibold">Name:</label>
+      <input
+        className="w-full p-1  bg-black/10 rounded-md focus:outline-none  focus:ring-0"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        style={{
+          border: isActive && name.length < 3 && "1px solid red",
+        }}
+      ></input>
+      <small
+        className="text-red-500 mb-1 "
+        style={{
+          visibility: isActive && name.length < 3 ? "visible" : "hidden",
+        }}
+      >
+        Name must be at least 3 characters long
+      </small>
+      <label className="block text-sm font-semibold mt-1">Phone:</label>
+      <input
+        className="w-full p-1  bg-black/10 rounded-md focus:outline-none focus:ring-0 "
+        style={{
+          border: isActive && phone.length !== 10 && "1px solid red",
+        }}
+        value={phone}
+        type="number"
+        onChange={(e) => setPhone(e.target.value)}
+      ></input>
+      <small
+        className="text-red-500 "
+        style={{
+          visibility: isActive && phone.length !== 10 ? "visible" : "hidden",
+        }}
+      >
+        Phone must be avialable and must be 10 number{" "}
+      </small>
+    </>
+  );
+}
+function NumberOfPeople({
+  adults,
+  setAdults,
+  children,
+  setChildren,
+  infants,
+  setInfants,
+}) {
+  const numberOptions = Array.from({ length: 4 }, (_, i) => i);
+  return (
+    <>
+      <label className="block text-sm font-semibold mb-2 mt-2">
+        Number of Person:
+      </label>
+      <div className="flex justify-between  text-sm font-semibold">
+        <div>
+          <span>Adults: </span>
+          <select
+            className="p-1 bg-black/10 rounded-md w-20"
+            value={adults}
+            onChange={(e) => setAdults(e.target.value)}
+          >
+            {numberOptions.map((number) => (
+              <option>{number}</option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <span>Children: </span>
+          <select
+            className="p-1 bg-black/10 rounded-md w-20 "
+            value={children}
+            onChange={(e) => setChildren(e.target.value)}
+          >
+            {numberOptions.map((number) => (
+              <option>{number}</option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <span>Infants: </span>
+          <select
+            className="p-1 bg-black/10 rounded-md w-20"
+            value={infants}
+            onChange={(e) => setInfants(e.target.value)}
+          >
+            {numberOptions.map((number) => (
+              <option>{number}</option>
+            ))}
+          </select>
+        </div>
+      </div>
     </>
   );
 }
 
-function BookForm() {
-  const numberOptions = Array.from({ length: 5 }, (_, i) => i + 1);
+function TotalPrice() {
   return (
-    <div className="w-2/3 bg-purple-200 m-5 p-4">
-      <div className="flex  justify-between">
-        <h3 className="font-medium  text-2xl mt-2 mb-4">Fill Out The Form</h3>
-        <button className="text-2xl font-bold  rounded-sm p-1">✕</button>
-      </div>
-      <form>
-        <label className="block text-sm font-semibold">Name:</label>
-        <input className="w-full p-1 mb-3"></input>
-        <label className="block text-sm font-semibold">Email</label>
-        <input className="w-full p-1 mb-3"></input>
-        <label className="block text-sm font-semibold">Phone:</label>
-        <input className="w-full p-1 mb-3"></input>
-        <label className="block text-sm font-semibold mb-3">Person:</label>
-        <div className="flex justify-between w-3/4 text-sm font-semibold">
-          <div>
-            <span>Adults: </span>
-            <select className="p-1 bg-black/10 rounded-md">
-              {numberOptions.map((number) => (
-                <option>{number}</option>
-              ))}
-            </select>
-          </div>
-          <div>
-            <span>Children: </span>
-            <select className="p-1 bg-black/10 rounded-md">
-              {numberOptions.map((number) => (
-                <option>{number}</option>
-              ))}
-            </select>
-          </div>
-          <div>
-            <span>Infants: </span>
-            <select className="p-1 bg-black/10 rounded-md">
-              {numberOptions.map((number) => (
-                <option>{number}</option>
-              ))}
-            </select>
-          </div>
-        </div>
-        <button className="bg-white text-sm font-semibold rounded-xl p-2 w-[70px] m-5">
-          Book
-        </button>
-      </form>
-      <p>
-        total: <span>$0</span>
-      </p>
+    <p className="mt-8 text-lg font-semibold">
+      Total: <span>$0</span>
+    </p>
+  );
+}
+
+function NoSeatAlert({ onSetAlert }) {
+  return (
+    <div className="p-10 w-[400px] bg-rose-500 h-[200px]  text-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-lg ">
+      <p>Sorry, No Seats Available For This Flight</p>
+      <button
+        className="absolute top-2 right-3 text-2xl font-bold"
+        onClick={() => onSetAlert(false)}
+      >
+        ✕
+      </button>
+    </div>
+  );
+}
+
+function ErrorMessage() {
+  return (
+    <div className="text-center bg-purple-600  w-1/2 h-[20vh] p-5 rounded-md text-white flex flex-col justify-items-center  ">
+      <h2 className="mx-auto text-3xl mb-2">
+        <FaSadTear />
+      </h2>
+      <p className=" text-3xl font-semibold ">NETWORK ERROR!</p>
+      <small>Please Check Your Network</small>
     </div>
   );
 }
